5dca4b8230a0abe1e10d8e08b573a4fa417f1a92
xzy32 <xzy32@DESKTOP-ALI14LA>
2022-06-01 01:44:48 +0800

    加入專案檔案。

---
+++ MvcMovies/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovies.Controllers
+{
+    public class HelloWorldController : Controller
+    {// 
+     // GET: /MvcMovies/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /MvcMovies/Welcome/ 
+
+        public ActionResult Welcome(string name, int numtimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numtimes;
+            return View();
+        }
+    }
+}
---
+++ MvcMovies/Migrations/Configuration.cs
@@ -0,0 +1,55 @@
+﻿namespace MvcMovies.Migrations
+{
+    using MvcMovies.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcMovies.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MovieDBContext context)
+        {
+            context.Movies.AddOrUpdate(i => i.Title,
+            new Movie
+            {
+                Title = "When Harry Met Sally",
+                ReleaseDate = DateTime.Parse("1989-01-11"),
+                Genre = "Romantic Comedy",
+                Price = 7.99M,
+                Rating = "PG"
+            },
+            new Movie
+            {
+                Title = "Ghostbusters",
+                ReleaseDate = DateTime.Parse("1992-05-22"),
+                Genre = "Comedy",
+                Price = 9.99M,
+                Rating = "PG"
+            },
+
+            new Movie
+            {
+                Title = "Ghostbusters 2",
+                ReleaseDate = DateTime.Parse("1992-05-20"),
+                Genre = "Comedy",
+                Price = 9.99M,
+                Rating = "PG"
+            },
+            new Movie
+            {
+                Title = "Rio Bravo",
+                ReleaseDate = DateTime.Parse("2005-05-22"),
+                Genre = "Western",
+                Price = 3.99M,
+                Rating = "PG"
+            }
+           );
+        }
+    }
+}
